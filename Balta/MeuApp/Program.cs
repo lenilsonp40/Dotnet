98b8caa206450e8aba38e6f48c2ba33a5b92da12
Lenilson Soares <lenilsonp40@gmail.com>
2023-05-11 13:59:16 -0300

    first commit

---
+++ Balta/MeuApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x =  2;
+            x = 75 / 23;
+            Console.WriteLine(x);
+            
+        }
+    }
+}
+
+
+  
