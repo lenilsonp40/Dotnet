a32db10c24923b9a684c60c7581fc1fddc9f3fba
Lenilson Soares <lenilsonp40@gmail.com>
2023-05-22 20:35:09 -0300

    pares

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> numeros = new List<int>() { 15, 25, 36, 49, 60, 64, 18, 7, 2, 4 };
+        int resultadoPares = SomarNumerosPares(numeros);
+        Console.WriteLine("a soma de todos os números pares na lista: " + resultadoPares);
+    }
+
+    public static int SomarNumerosPares(List<int> numeros)
+    {
+        int soma = 0;
+        foreach (int numero in numeros)
+        {
+            if (numero % 2 == 0)
+            {
+                soma += numero;
+            }
+        }
+        return soma;
+    }
+}
