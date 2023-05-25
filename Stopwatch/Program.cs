aa2162dcfe2c9b4f9a7e053fc6f42c6c6ffa9d94
Lenilson Soares <lenilsonp40@gmail.com>
2023-05-25 17:47:01 -0300

    uP

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -13,12 +13,15 @@ namespace Stopwatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundo");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
             Console.WriteLine(data);
         }
 
