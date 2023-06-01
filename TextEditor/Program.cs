1440bb2d382c158780866510c36a0d5c3901104f
Lenilson Soares <lenilsonp40@gmail.com>
2023-06-01 17:40:30 -0300

    escape

---
+++ TextEditor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sar");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0 :System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir() { }
+
+        static void Editar() { 
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair!)");
+            Console.WriteLine("----------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+
+            }
+
+
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write(text);
+        }
+
+    }
+}
