49e9a04bea8bbd5a7bb20ce6e8f9ce6c993938d3
Lenilson Soares <lenilsonp40@gmail.com>
2023-08-05 20:35:50 -0300

    controller

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,6 +1,15 @@
+using Blog.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogDataContext>();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "");
+app.MapControllers();
+
+
 
 app.Run();
